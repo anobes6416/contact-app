0509f7279a99fe8922839c17146ed239549488a4
anobes6416 <sebonaifa777@gmail.com>
2023-11-06 14:10:47 -0500

    contact-app

---
+++ src/components/AddContact.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AddContact = () => {
+    return(
+        <div className="ui main">
+            <h2>Add Contact</h2>
+            <form className="ui form">
+                <div className="field">
+                    <label>Name</label>
+                    <input type="text" name="name" placeholder="Name"/>
+                </div>
+                <div className="field">
+                    <label>Email</label>
+                    <input type="text" name="email" placeholder="Email"/>
+                    <button className="ui button blue">Add</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default AddContact;
